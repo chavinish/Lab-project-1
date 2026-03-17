b4f50908b233958fce8b1dfb0e81d1a49bef1a9b
Chavini <chavinish@gmail.com>
2026-03-17 15:34:02 +0530

    Update Lab1.py

--- Lab1.py
+++ Lab1.py
@@ -5,4 +5,16 @@ print ("Kothalawala college")
 a multiline 
 comment'''
 # we use comments to understand the code clearly
+print ("22 divided by 7 is :",22/7)
+#concatenation mean joining things together usually strings
+
+operation = "Sum"
+total =8
+print (operation + "is" + str(total))
+
+#area of a rectangle
+length =10
+width = 5
+area = length * width
+print ("Area of a rectangle with length" + str(length) + "and width" + str (width)+ "is" + str(area))
 
